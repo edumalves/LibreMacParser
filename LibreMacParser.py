f99526ff2c948269453dfbe952198f70854874e2
Eduardo Alves <ealves@me.com>
2021-07-28 18:11:01 -0300

    Basic Funcionality Ready

--- LibreMacParser.py
+++ LibreMacParser.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
 
-rawdata = pd.read_csv("EduardoAlves_glucose.csv")
-rawdata
+rawdata = pd.read_csv("EduardoAlves_glucose.csv", header=1, parse_dates=True, index_col='Device Timestamp', infer_datetime_format=True)
+
+last_import_date = datetime.datetime(2021,6,24,9,14,0)
+
+new_logs = rawdata[rawdata.index >= last_import_date]
+
+new_logs.to_csv('GlucoseExport.csv')

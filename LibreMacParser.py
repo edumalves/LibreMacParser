9bde1a5c48ed3036d9b1adaa68a0b0a9758af60a
Eduardo Alves <ealves@me.com>
2021-07-29 13:46:44 -0300

    added Null values filtering logic

--- LibreMacParser.py
+++ LibreMacParser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 import dateparser as dp
 
 rawdata = pd.read_csv("EduardoAlves_glucose.csv", header=1, parse_dates=True, index_col='Device Timestamp', infer_datetime_format=True)
@@ -7,17 +8,27 @@ dateonfile = datekeeper.read()
 
 last_export_date = dp.parse(dateonfile)
 
-new_logs = rawdata[rawdata.index >= last_export_date]
+new_logs = rawdata[rawdata.index > last_export_date]
 
-new_export_date = new_logs.index[-1]
+hist_glucose = new_logs['Historic Glucose mg/dL'].notnull() == True
+scan_glucose = new_logs['Scan Glucose mg/dL'].notnull() == True
 
-new_logs.to_csv('GlucoseExport.csv')
+filtered_logs = new_logs[scan_glucose | hist_glucose]
 
-datekeeper.close
-datekeeper = open("LibreMacParser.ini", "w")
-datekeeper.write(str(new_export_date))
-datekeeper.close
+try:
+    new_export_date = filtered_logs.index[-1]
 
-print("Export file successfully generated.")
-print("Export data from", str(dateonfile))
-print("Last data exported is as of", str(new_export_date))
+    filtered_logs.to_csv('GlucoseExport.csv')
+
+    datekeeper.close
+    datekeeper = open("LibreMacParser.ini", "w")
+    datekeeper.write(str(new_export_date))
+    datekeeper.close
+
+    print("Export file successfully generated.")
+    print("Export data from", str(dateonfile))
+    print("Last data exported is as of", str(new_export_date))
+
+except IndexError:
+    print("No new entries to process.")
+    
